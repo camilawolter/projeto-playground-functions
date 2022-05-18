65c2218643e56f011b95009770a5a927e6e2d331
Camila Wolter <camila0wolter@gmail.com>
2022-05-18 18:49:03 -0300

    adicionando desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,9 +4,23 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let resultado;
+  
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) {
+    resultado = true;
+  } else {
+    resultado = false;
+  }
+  
+  if (lineA > Math.abs(lineB- lineC) && lineB > Math.abs(lineC- lineA) && lineC > Math.abs(lineA - lineB)) {
+    resultado = true;
+  } else {
+    resultado = false;
+  }
+  return resultado;
 }
+console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
